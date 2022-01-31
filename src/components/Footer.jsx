2e4f13902fa92d4785fa3f9a668616127f5f2f9f
AlvaJufinto <yeahbruh199@gmail.com>
2022-01-31 23:16:53 +0700

    Footer done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,9 +8,47 @@ import { StyledLink, StyledSection, StyledButton } from "../ReuseableComponents/
 import WhatsappIcon from "./../assets/svg/Whatsapp-icon.svg";
 
 const FooterSection = styled.div`
-    min-height: 450px;
+    padding: 50px 0px;
+    height: 400px;
     width: 100%; 
     background-color: ${GlobalColors.hardGrey};
+    display: flex;
+    align-items: center;
+    
+    .Icon {
+        margin: 0 10px 0 0;
+    }
+
+    p {
+        font-size: 1.20rem;
+    }
+
+    ${StyledSection} {
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        /* background: red; */
+        
+        .LeftSection {
+            height: 100%;
+            width: 28%;
+            /* background: orange; */
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+        }
+
+        .RightSection {
+            /* background: green; */
+            width: 28%;
+
+            ${StyledButton} {
+                text-align: left;
+                width: 100%;
+            }
+        }
+    }
 `
 
 const Footer = () => {
@@ -28,15 +66,18 @@ const Footer = () => {
                     </div>
                 </div>
                 <div className="RightSection">
-                    <p><YouTubeIcon /> smkn26jktofficial</p>
-                    <p><Twitter /> smkn26jktofficial</p>
-                    <p><Instagram /> smkn26jktofficial</p>
+                    <p><YouTubeIcon className="Icon" /> smkn26jktofficial</p>
+                    <p><Twitter className="Icon" /> smkn26jktofficial</p>
+                    <p><Instagram className="Icon" /> smkn26jktofficial</p>
                     <StyledButton 
+                        variant="success"
                         background={GlobalColors.green}
-                        width="auto"
+                        // width="200"
                         height="auto"
                         >
-                        <img src={WhatsappIcon} /> Hubungi Admin
+                        <img 
+                            src={WhatsappIcon} 
+                            className="Icon" /> Hubungi Admin
                     </StyledButton>
                 </div>
             </StyledSection>
