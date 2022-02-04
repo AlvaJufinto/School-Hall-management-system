1c94b4e6998fd3021bb05f58dcc9de38d15541df
AlvaJufinto <yeahbruh199@gmail.com>
2022-02-04 15:53:10 +0700

    Making form

---
+++ src/components/BankGroup.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+import { GlobalMeasurements, GlobalColors } from '../globals';
+
+import MandiriLogo from "./../assets/img/mandiri-logo.png";
+import BcaLogo from "./../assets/img/mandiri-logo.png";
+import MandiriLogo from "./../assets/img/mandiri-logo.png";
+
+
+const BankGroup = () => {
+    return (
+        <BankGroup>
+
+        </BankGroup>
+    )
+}
+
+export default BankGroup
--- src/pages/FormOrder.jsx
+++ src/pages/FormOrder.jsx
@@ -1,13 +1,23 @@
 import styled from 'styled-components';
+import { Form } from "react-bootstrap";
 
 import StyledNavbar from "../components/Navbar";
 import CardComponent from "../components/Card";
 import Footer from "../components/Footer";
 
 import { StyledSection, StyledTitle } from '../ReuseableComponents/ReuseableComponents';
+import { GlobalFonts, GlobalColors } from '../globals';
 
 const StyledForm = styled.form`
-    
+
+    .FormGroups {
+        color: black;
+        font-family: ${GlobalFonts.secondary};
+
+        ${Form.Group} {
+            font-weight: 500;
+        }
+    }
 `
 
 const FormOrder = () => {
@@ -26,14 +36,43 @@ const FormOrder = () => {
                     textDecoration: 'underline'
                 }} >Formulir pemesanan aula</StyledTitle>
                 <StyledForm>
-
+                    <div className="TopForm">
+                        <CardComponent 
+                            image={data.image} 
+                            title={data.title}
+                            packet={data.packet}
+                            price={data.price}
+                        />
+                        <div className="FormGroups">
+                            <Form.Group className="mb-3" controlId="formGroupEmail">
+                                <Form.Label>Atas Nama</Form.Label>
+                                <Form.Control type="text" />
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="formGroupPassword">
+                                <Form.Label>Nama Acara</Form.Label>
+                                <Form.Control type="text" />
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="formGroupEmail">
+                                <Form.Label>Email</Form.Label>
+                                <Form.Control type="email" />
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="formGroupPassword">
+                                <Form.Label>No Whatsapp</Form.Label>
+                                <Form.Control type="number" />
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="formGroupEmail">
+                                <Form.Label>Jumlah porsi</Form.Label>
+                                <Form.Control type="number"  />
+                            </Form.Group>
+                            <Form.Group className="mb-3" controlId="formGroupPassword">
+                                <Form.Label>Pilih tanggal</Form.Label>
+                                <Form.Control type="date" />
+                            </Form.Group>
+                        </div>
+                        
+                    </div>
                 </StyledForm>
-                <CardComponent 
-                    image={data.image} 
-                    title={data.title}
-                    packet={data.packet}
-                    price={data.price}
-                />
+                
             </StyledSection>
             <Footer />
         </>
