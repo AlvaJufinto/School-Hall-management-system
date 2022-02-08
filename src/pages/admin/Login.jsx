c3b06ef47ecdd314b96863b61cdc6611a6cd0787
AlvaJufinto <yeahbruh199@gmail.com>
2022-02-08 22:07:51 +0700

    Developing login page

--- src/pages/admin/Login.jsx
+++ src/pages/admin/Login.jsx
@@ -1,31 +1,55 @@
 import styled from 'styled-components';
-import { Form, Button } from "react-bootstrap";
+import { Form } from "react-bootstrap";
 
-import { GlobalColors } from "../../globals";
+import { StyledButton } from '../../ReuseableComponents/ReuseableComponents';
+import { GlobalColors, GlobalFonts } from "../../globals";
 
 const LoginContainer = styled.div`
     height: 100vh;
     display: flex;
     align-items: center;
     justify-content: center;
-`
+    background: ${GlobalColors.hardGrey};
+    font-family: ${GlobalFonts.secondary};
+
+`;
+
+const StyledForm = styled.form`
+    background: #000000;
+    width: 500px;
+    max-width: 90%;
+    height: 400px;
+
+    h2 {
+        color: ${GlobalColors.violet};
+    }
+`;
 
 const Login = () => {
     return (
         <LoginContainer>
-            <Form>
+            <StyledForm className="StyledForm" >
+                <h2>Sewa Aula</h2>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Control 
+                        type="text" 
+                        placeholder="Username" />
                 </Form.Group>
-
                 <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control 
+                        type="password" 
+                        placeholder="Password" />
                 </Form.Group>
-
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
-            </Form>
+                
+                <StyledButton 
+                    color={GlobalColors.white}
+                    background={GlobalColors.violet}
+                    fontSize="1.5"
+                    borderRadius="15"
+                    type="submit">
+                        Buat Pesanan
+                </StyledButton>
+            </StyledForm>
         </LoginContainer>
     )
 }
