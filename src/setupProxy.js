e07ce795da8308d32b9cac10479799b20c596e0e
AlvaJufinto <yeahbruh199@gmail.com>
2022-02-10 06:56:41 +0700

    yes

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    '/api',
+    '/',
     createProxyMiddleware({
       target: 'https://sewa-aula-server.herokuapp.com/',
       changeOrigin: true,
