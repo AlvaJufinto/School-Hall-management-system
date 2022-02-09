c4affbb5a117eb7f3a45db26a283335649cb2654
AlvaJufinto <yeahbruh199@gmail.com>
2022-02-09 07:21:04 +0700

    Admin login UI is done

--- src/pages/admin/Login.jsx
+++ src/pages/admin/Login.jsx
@@ -9,45 +9,76 @@ const LoginContainer = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
-    background: ${GlobalColors.hardGrey};
+    background: ${GlobalColors.grey};
     font-family: ${GlobalFonts.secondary};
-
 `;
 
 const StyledForm = styled.form`
-    background: #000000;
+    border-radius: 10px;
+    background: white;
     width: 500px;
     max-width: 90%;
     height: 400px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding: 50px;
 
     h2 {
         color: ${GlobalColors.violet};
+        text-align: center;
+        margin: 0 0 25px 0;
+    }
+
+    .FormText {
+        background-color: white;
+        color: black;
+        border: none;
+        box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.15) !important; 
+    }
+
+    ${StyledButton} {
+        height: auto;
+        width: 100%;
+        padding: 10px;
+        box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.25) !important; 
+    }
+
+    @media (max-width: 768px) {
+        padding: 20px;
     }
 `;
 
 const Login = () => {
+    const LoginHandler = (e) => {
+
+    }
+
     return (
         <LoginContainer>
             <StyledForm className="StyledForm" >
                 <h2>Sewa Aula</h2>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Control 
+                        variant="dark"
                         type="text" 
-                        placeholder="Username" />
+                        placeholder="Username"
+                        className="FormText" />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Control 
                         type="password" 
-                        placeholder="Password" />
+                        placeholder="Password"
+                        className="FormText" />
                 </Form.Group>
                 
                 <StyledButton 
                     color={GlobalColors.white}
                     background={GlobalColors.violet}
-                    fontSize="1.5"
-                    borderRadius="15"
+                    fontSize="1.25"
+                    borderRadius="5"
                     type="submit">
-                        Buat Pesanan
+                        Login
                 </StyledButton>
             </StyledForm>
         </LoginContainer>
